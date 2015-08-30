73465559d7961aebf26896ac9aad62a6dc6f1384
Igor Popov <ihptru@gmail.com>
2015-08-30 13:06:03 +0000

    re-make feedback page

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -5,7 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>"Глобал Тендер" CЕМИНАРЫ ПО ГОСУДАРСТВЕННЫМ ЗАКУПКАМ</title>
         <meta name="description" content="">
-        <link rel="stylesheet" href="{{URL::to('assets/css/jquery.fancybox-1.3.4.css')}}" type="text/css" media="screen" />
+
+        <script type="text/javascript" src="{{URL::to('assets/js/highslide/highslide-with-gallery.packed.js')}}" ></script>
+        <script type="text/javascript" src="{{URL::to('assets/js/highslide/highslide.config.js')}}" ></script>
+        <link href="{{URL::to('assets/js/highslide/highslide.css')}}" rel="stylesheet">
 @stop
 
 @section('content')
@@ -15,47 +18,46 @@
                     </div>
                 </div>
                 <main class="wrapper clearfix">
-                    <!-- <a href="#" class="nav-next"><i class="fa fa-angle-right"></i></a>
-                    <a href="#" class="nav-prev"><i class="fa fa-angle-left"></i></a> -->
-                    <div class="feed-imgs">
-			<img src="assets/img/feedback/17.07.2015.jpg">
-			<img src="assets/img/feedback/000.jpg"><!--
-                        --><img src="assets/img/feedback/002.jpg"><!--
-                        --><img src="assets/img/feedback/f34.jpg"><!--
-                        --><img src="assets/img/feedback/001.jpg"><!--
-                        --><img src="assets/img/feedback/f1.jpg"><!--
-                        --><img src="assets/img/feedback/f2.jpg"><!--
-                        --><img src="assets/img/feedback/f3.jpg"><!--
-                        --><img src="assets/img/feedback/f4.jpg"><!--
-                        --><img src="assets/img/feedback/f5.jpg"><!--
-                        --><img src="assets/img/feedback/f6.jpg"><!--
-                        --><img src="assets/img/feedback/f7.jpg"><!--
-                        --><img src="assets/img/feedback/f8.jpg"><!--
-                        --><img src="assets/img/feedback/f9.jpg"><!--
-                        --><img src="assets/img/feedback/f10.png"><!--
-                        --><img src="assets/img/feedback/f11.png"><!--
-                        --><img src="assets/img/feedback/f12.jpg"><!--
-                        --><img src="assets/img/feedback/f13.jpg"><!--
-                        --><img src="assets/img/feedback/f14.jpg"><!--
-                        --><img src="assets/img/feedback/f15.jpg"><!--
-                        --><img src="assets/img/feedback/f16.jpg"><!--
-                        --><img src="assets/img/feedback/f17.jpg"><!--
-                        --><img src="assets/img/feedback/f18.jpg"><!--
-                        --><img src="assets/img/feedback/f19.jpg"><!--
-                        --><img src="assets/img/feedback/f20.jpg"><!--
-                        --><img src="assets/img/feedback/f21.jpg"><!--
-                        --><img src="assets/img/feedback/f22.jpg"><!--
-                        --><img src="assets/img/feedback/f23.jpg"><!--
-                        --><img src="assets/img/feedback/f24.jpg"><!--
-                        --><img src="assets/img/feedback/f25.jpg"><!--
-                        --><img src="assets/img/feedback/f26.jpg"><!--
-                        --><img src="assets/img/feedback/f27.jpg"><!--
-                        --><img src="assets/img/feedback/f28.jpg"><!--
-                        --><img src="assets/img/feedback/f29.jpg"><!--
-                        --><img src="assets/img/feedback/f30.jpg"><!--
-                        --><img src="assets/img/feedback/f31.jpg"><!--
-                        --><img src="assets/img/feedback/f32.jpg"><!--
-                        --><img src="assets/img/feedback/f33.jpg">
+                    <div class="feed-imgs highslide-gallery">
+                        <a class="highslide feedback-img" href="assets/img/feedback/17.07.2015.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_17.07.2015.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/000.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_000.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/002.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_002.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f34.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f34.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/001.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_001.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f1.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f1.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f2.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f2.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f3.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f3.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f4.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f4.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f5.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f5.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f6.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f6.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f7.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f7.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f8.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f8.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f9.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f9.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f10.png" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f10.png"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f11.png" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f11.png"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f12.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f12.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f13.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f13.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f14.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f14.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f15.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f15.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f16.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f16.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f17.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f17.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f18.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f18.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f19.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f19.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f20.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f20.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f21.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f21.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f22.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f22.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f23.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f23.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f24.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f24.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f25.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f25.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f26.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f26.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f27.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f27.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f28.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f28.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f29.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f29.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f30.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f30.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f31.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f31.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f32.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f32.jpg"></a>
+                        <a class="highslide feedback-img" href="assets/img/feedback/f33.jpg" onclick="return hs.expand(this, config1);"><img src="assets/img/feedback/sml/sml_f33.jpg"></a>
+
                     </div>
                 </main>
 @stop
@@ -63,18 +65,21 @@
 @section('script')
     <script type="text/javascript" src="{{URL::to('assets/js/vendor/jquery.fancybox-1.3.4.js')}}"></script>
     <script>
-        $('img').each(function(){
+
+        $('.highslide-credits').html("")
+
+        /*$('img').each(function(){
             $(this).after('<a href="' + $(this).attr('src') + '" rel="gallery" style="background-image: url(' + $(this).attr('src') + ');" target="_blank" class="feedback-img"></a>');
             $(this).remove();
-        });
+        });*/
 
-        $(function(){
+        /*$(function(){
             $('.feedback-img').fancybox({
                 width : 1287,
                    height : 720,
                    fitToView : false,
                    autoSize : false
             });
-        });
+        });*/
     </script>
 @stop
