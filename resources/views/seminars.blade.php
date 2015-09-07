66afc25a3eb0ff72c2f526bc913e731b7864cb8c
Igor Popov <ihptru@gmail.com>
2015-09-07 18:12:00 +0000

    seminar changes and program for yaroslavl223

--- resources/views/seminars.blade.php
+++ resources/views/seminars.blade.php
@@ -91,6 +91,7 @@ $semArray = array(
 	'surgut' => array('Сургут', '25 сентября', '15', 'http://www.surgeo.ru/wp-content/uploads/2012/02/%D0%92%D0%B8%D0%B4-%D0%BD%D0%B0-%D0%93%D1%80%D0%B5%D1%81-%D0%A1%D1%83%D1%80%D0%B3%D1%83%D1%82-%D0%A1%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D0%B0%D1%8F-%D0%92%D0%B5%D0%BD%D0%B5%D1%86%D0%B8%D1%8F.jpg'),
 	'rnd' => array('Ростов-на-Дону', '29 сентября', '15', 'http://www.worldlab.co/wp-content/uploads/2015/02/%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2.jpg'),
 	'piter' => array('Санкт-Петербург', '02 октября', '15', 'http://ved69.ru/images/Spb.jpg'),
+	'yaroslavl223' => array('Ярославль', '12 октября', '15', 'http://ic.pics.livejournal.com/drugoi/484155/1892625/1892625_original.jpg'),
         ),
     '44'  => array(
 	//'belgorod' => array('Белгород', '14 августа', '15', 'http://trans-continental.ru/wp-content/uploads/2013/12/41907831.jpg'),
