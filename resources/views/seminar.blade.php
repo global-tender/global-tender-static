daaf5e1b1fa34332b2dd9b58d280399755c8ee06
Igor Popov <ihptru@gmail.com>
2015-10-22 10:12:30 +0000

    seminar changes

--- resources/views/seminar.blade.php
+++ resources/views/seminar.blade.php
@@ -19,7 +19,7 @@ $semArray = array(
 	'kazan223' => array('Казань', '09 ноября', '15', 'http://mir-travel.com/images/content/destinations/%D0%9A%D0%B0%D0%B7%D0%B0%D0%BD%D1%8C8.jpg'),
 	'xab' => array('Хабаровск', '23 ноября', '15', 'http://organum-visus.com/sites/default/files/styles/1024x768/public/news_mini_top_img/khabarovsk-organum-visus-eyenews-120712g.jpeg?itok=C7U8OkzE&slideshow=true&slideshowAuto=false&slideshowSpeed=4000&speed=350&transition=fade'),
         'vlad223' => array('Владивосток', '24 ноября', '15', 'http://s.pikabu.ru/post_img/big/2013/10/23/5/1382508326_1790634624.jpg'),
-        'blag' => array('Благовещенск', '24 ноября', '15', 'http://www.infokart.ru/wp-content/uploads/2013/02/blagoveshchensk_gorod.jpg'),
+        'blag' => array('Благовещенск', '25 ноября', '15', 'http://www.infokart.ru/wp-content/uploads/2013/02/blagoveshchensk_gorod.jpg'),
     ),
     '44'  => array(
 	'stav44' => array('Ставрополь', '30 ноября', '15', 'http://nstarikov.ru/wp-content/uploads/2014/11/%D1%81%D1%82%D0%B0%D0%B2%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D1%8C.jpg'),        
